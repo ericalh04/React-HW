9b6d069b7aa928f2d15a9a2e318a6efb4e8f0ea4
Erica Honadel <ericalh04@gmail.com>
2024-04-01 16:40:45 -0400

    react hw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+
+const projects = [
+  { title: "Project 1", content: "Project 1 Description" },
+  { title: "Project 2", content: "Project 2 Description" },
+  { title: "Project 3", content: "Project 3 Description" },
+  { title: "Project 4", content: "Project 4 Description" }
+];
+
+function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <li className='navbar-name'>
+            Erica Honadel
+          </li>
+          <ul className='nav-menu'>
+            <li className='nav-item'>
+              About Me
+            </li>
+            <li className='nav-item'>
+              Projects
+            </li>
+            <li className='nav-item'>
+                Contact
+            </li>            
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+      <h1>Hello!</h1>
+      <p>My name is Erica and I am a freshman studying computer science and math at the University of Maryland!</p>
+    </div>
+  );
+}
+
+function Projects({ title, content }) {
+  return (
+    <>
+      <div className="project">
+        <h3 className='label'>{title}</h3>
+        <p className='label'>{content}</p>
+      </div>
+    </>    
+  );
+}
+
+function Contact() {
+  return (
+    <div className='container'>
+      <h1 className='label'>Contact Information</h1>
+      <p className='label'>Personal Email: ericalh04@gmail.com</p>
+      <p className='label'>School Email: ehonade1@terpmail.umd.edu</p>
+      <p className='label'>Phone: 443-619-2860</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <>
+      <div className='footer'>
+        <p>Erica Honadel</p>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Homepage></Homepage>
+      <h1 className="label">Projects</h1>
+      <div className="projects">
+        
+        {/* Mapping the information from the database to separate notes */}
+          {projects.map((element) => (
+            <Projects title={element.title} content={element.content} />
+          ))}
+      </div>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
